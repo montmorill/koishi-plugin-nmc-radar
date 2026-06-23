19b15c0dd3d6e2ea8ecf435db169246af55a1b4b
montmorill <montmorill@outlook.com>
2026-06-23 19:50:57 +0800

    Improve error message for non-existent radar station

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ export function apply(ctx: Context) {
     .action(async ({ session }, name) => {
       const url = (radars as Dict<string>)[name]
       if (!url)
-        return void session?.send('该雷达图不存在')
+        return void session?.send('该雷达站不存在，可使用 radar.list 查看所有雷达站')
       const dom = new JSDOM(await ctx.http.get(url))
       const image = dom.window.document.querySelector('div[data-img]')
       return h('img', { src: (image as HTMLElement)?.dataset.img || '' })
