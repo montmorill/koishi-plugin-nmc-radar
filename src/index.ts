6bb2b316be7bdd42600530e694adfe395227220b
montmorill <montmorill@outlook.com>
2026-06-29 00:18:57 +0800

    Refactor resolver functions to use Product interface and enhance command options for radar

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,15 @@ export const inject = {
   optional: ['ffmpeg'],
 }
 
-type Resolver = (urls: string[], id: string) => Awaitable<h>
+interface Product {
+  url: string
+  time: string
+}
+
+type Resolver = (products: Product[], id: string) => Awaitable<h>
 const resolvers: Record<string, Resolver> = {
-  img: urls => h.img(urls[0]),
-  imgs: urls => h(h.Fragment, ...urls.map(url => h.img(url))),
-  url: urls => h.text(urls[0]),
-  urls: urls => h.text(urls.join('\n')),
+  img: products => h(h.Fragment, ...products.map(url => h.img(url.url))),
+  url: products => h.text(products.map(url => url.url).join('\n')),
 }
 
 export interface Config {
@@ -30,30 +33,37 @@ export const Config: Schema<Config> = Schema.object({
 export function apply(ctx: Context, config: Config) {
   const command = ctx.command('radar <name:string>', '查看雷达图', { checkUnknown: true })
     .alias('雷达')
+    .option('reverse', '-r 反转顺序')
+    .option('count', '-n <count:number> 最大输出数量')
     .option('type', '--type <type:string> 输出类型', { type: Object.keys(resolvers) })
-    .option('type', '--img 输出单张图片', { value: 'img' })
-    .option('type', '--imgs 输出多张图片', { value: 'imgs' })
+    .option('type', '--img 输出图片', { value: 'img' })
     .option('type', '--url 输出 URL', { value: 'url' })
-    .option('type', '--urls 输出 URL 列表', { value: 'urls' })
     .action(async ({ session, options }, name) => {
       if (!(name in radars))
         return void session?.send('雷达站不存在，可使用 radar.list 查看所有雷达站。')
 
+      const type = options?.type || config.defaultResolver
+      const count = options?.count ?? (type === 'img' ? 1 : undefined)
+
       const url = radars[name as keyof typeof radars]
       const { window: { document } } = new JSDOM(await ctx.http.get(url))
       const nodes = document.querySelectorAll<HTMLElement>('div[data-img]')
-      const urls = Array.from(nodes).map(node => node.dataset.img || '')
-      const resolver = resolvers[options?.type || config.defaultResolver]
-      return await resolver(urls, Random.id())
+      const products = Array.from(nodes).slice(0, count).map(node => ({
+        url: node.dataset.img!,
+        time: node.dataset.time!, // MM/DD HH:mm
+      }))
+      if (!options?.reverse)
+        products.reverse()
+      return await resolvers[type](products, Random.id())
     })
 
   ctx.inject(['ffmpeg'], async (ctx) => {
     command.option('type', '--gif 输出 GIF 动画', { value: 'gif' })
-    resolvers.gif = async (urls, id) => {
+    resolvers.gif = async (products, id) => {
       const baseDir = path.join(ctx.baseDir, 'temp', name, id)
       await mkdir(baseDir, { recursive: true })
 
-      await Promise.all(urls.map(async (url, i) => {
+      await Promise.all(products.map(async ({ url }, i) => {
         const response = await ctx.http.get(url, { responseType: 'stream' })
         const filename = `${String(i + 1).padStart(3, '0')}.png`
         const filePath = path.join(baseDir, filename)
@@ -65,7 +75,7 @@ export function apply(ctx: Context, config: Config) {
         .input(path.join(baseDir, '%03d.png'))
         .run('file', outputPath)
 
-      return h.img(`file:///${outputPath}`)
+      return h.img(`file://${outputPath}`)
     }
   })
 
